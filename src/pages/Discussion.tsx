094144680611cc358cf8b32904f733e33302e789
DEVARASHETTY NITHISH CHANDRA AZAD <nithishchandra16@gmail.com>
2025-08-21 09:29:40 +0530

    Fix Discussion component export

--- src/pages/Discussion.tsx
+++ src/pages/Discussion.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Discussion: React.FC = () => {
+export const Discussion: React.FC = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold text-gray-800 mb-6">Discussion</h1>
@@ -12,5 +12,3 @@ const Discussion: React.FC = () => {
     </div>
   );
 };
-
-export default Discussion;
